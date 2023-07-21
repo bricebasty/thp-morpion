0477c38d2d2a5f5bbc9457fd3cbdc09da04d97b2
celgost <brice.basty1@gmail.com>
2023-07-21 10:17:41 +0200

    oui

--- app.rb
+++ app.rb
@@ -8,34 +8,36 @@ require_relative 'lib/player'
 require_relative 'lib/game'
 require_relative 'lib/board'
 
-# Defining the play method which drives the game
-def play
-  retry_flag = true # A flag that will determine if the game is to be replayed
-  game = Game.new # Initializes a new game
+# Defining the Application class which drives the game
+class Application
+  def initialize
+    retry_flag = true # A flag that will determine if the game is to be replayed
+    game = Game.new # Initializes a new game
 
-  # The main game loop that will keep running as long as players want to replay the game
-  while retry_flag
-    game.reset_board # Resets the board at the start of each game
+    # The main game loop that will keep running as long as players want to replay the game
+    while retry_flag
+      game.reset_board # Resets the board at the start of each game
 
-    # This loop runs for each round of the game until the game finishes
-    until game.finished?
-      game.ask_whos_next # Asks for the next player's turn
-      game.show_board # Displays the current state of the board
-      # Asks for the next move from the current player and makes that move
-      game.make_current_player_draw_in_cell(game.ask_which_cell)
-      game.switch_player # Switches the turn to the other player
+      # This loop runs for each round of the game until the game finishes
+      until game.finished?
+        game.ask_whos_next # Asks for the next player's turn
+        game.show_board # Displays the current state of the board
+        # Asks for the next move from the current player and makes that move
+        game.make_current_player_draw_in_cell(game.ask_which_cell)
+        game.switch_player # Switches the turn to the other player
+      end
+
+      # Once a game finishes, display the final state of the board
+      game.show_board
+
+      game.end # Ends the current game and displays the winner
+      game.show_games_played # Displays the total number of games played so far
+      game.show_winning_count # Displays the total number of wins for each player
+
+      # Asks if the players want to replay the game, if they do, the main game loop will run again
+      retry_flag = game.retry?
     end
-
-    # Once a game finishes, display the final state of the board
-    game.show_board
-
-    game.end # Ends the current game and displays the winner
-    game.show_games_played # Displays the total number of games played so far
-    game.show_winning_count # Displays the total number of wins for each player
-
-    # Asks if the players want to replay the game, if they do, the main game loop will run again
-    retry_flag = game.retry?
   end
 end
 
-play  # Start the game
+Application.new  # Start the game
